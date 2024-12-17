c1ff286403eec71564b3c3ec1def3c2b20b18cc4
muratgore <m.muratgore@gmail.com>
2024-12-17 23:44:39 +0300

    added entitytype configration

---
+++ src/Services/Report/Report.API/Infrastructure/EntityConfigrations/ReportItemEntityTypeConfigration.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Report.API.Domain.Entities;
+
+namespace Report.API.Infrastructure.EntityConfigrations;
+
+public class ReportItemEntityTypeConfigration : IEntityTypeConfiguration<ReportItem>
+{
+    public void Configure(EntityTypeBuilder<ReportItem> builder)
+    {
+        builder.HasKey(b=>b.Id);
+        
+        builder.Property(b => b.Location)
+            .IsRequired();
+        
+        builder.Property(b=> b.PhoneNumberCount)
+            .IsRequired();
+
+        builder.Property(b => b.HotelCount)
+            .IsRequired();
+        
+        builder.Property(b=> b.Status)
+            .IsRequired();
+    }
+}
